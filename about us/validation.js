7ea4b8b87860fd34f3d7c61814083fa53cf96e15
stevemkd <stephintomy2000@gmail.com>
2023-09-09 16:01:54 +0530

    Update validation.js

--- about us/validation.js
+++ about us/validation.js
@@ -1,19 +1,69 @@
-let input =document.getElementById('email')
+const form = document.getElementById('form');
+const name1 = document.getElementById('name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const msg = document.getElementById('msg');
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
+    validateInputs();
+});
 
- 
-input.onblur = function(){
-   
-    if(!input.value.includes("@")){
-        
-    
-        input.style.color="red";
-        alert('@ missing')
-    }
-    else{
-        input.style.color="#00ABE4"
-    
-    }
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
 }
 
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const nameValue =   name1.value.trim();
+    const emailValue = email.value.trim();
+    const sub = subject.value.trim();
+    const msgv = msg.value.trim();
+
+    if(nameValue === '') {
+        setError(name1, 'name is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(sub === '') {
+        setError(subject, 'subject is required');
+    
+    } else {
+        setSuccess(subject);
+    }
+
+    if(msgv === '') {
+        setError(msg, 'Write a message');
+    }  else {
+        setSuccess(msg);
+    }
+
+};
