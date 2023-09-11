e363d11ee1980c390e5389e15430413c1fd25e59
stevemkd <stephintomy2000@gmail.com>
2023-09-11 10:45:08 +0530

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,35 +1,60 @@
-let input =document.getElementById('email')
-let password= document.getElementById('password')
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
 
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
- 
-input.onblur = function(){
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+
    
-    if(!input.value.includes("@")){
-        
-    
-        input.style.color="red";
-        alert('@ missing')
-        
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
     }
-    else{
-        input.style.color="green"
-    
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
     }
-}
 
-password.onblur=function(){
-    if(password==null){
-        alert('enter password');
-        password.style.color="red";
-    }
-    else{
+   
 
-        password.style.color="green"
-
-    }
-}
-
-
-
+};
